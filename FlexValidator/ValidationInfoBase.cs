286233ccd160063bf59e961a2005fe0d3678d922
Dieter Van Broeck <VLM\OrdinaDVB>
2019-01-16 14:30:13 +0100

    Message is now optional

--- FlexValidator/ValidationInfoBase.cs
+++ FlexValidator/ValidationInfoBase.cs
@@ -2,19 +2,24 @@
 
 namespace FlexValidator {
     public class ValidationInfoBase {
+        public ValidationInfoBase(string guid) {
+            Guid = new Guid(guid);
+            Message = null;
+        }
+
         public ValidationInfoBase(string guid, string message) {
             Guid = new Guid(guid);
             Message = message;
         }
 
+        public ValidationInfoBase(Guid guid) {
+            Guid = guid;
+            Message = null;
+        }
+
         public ValidationInfoBase(Guid guid, string message) {
             Guid = guid;
             Message = message;
-        }
-
-        internal ValidationInfoBase(Guid guid) {
-            Guid = guid;
-            Message = null;
         }
 
         public Guid Guid { get; }
