b0b435efcb5bc14000003a95baeb4040b5259eed
Dieter Van Broeck <VLM\OrdinaDVB>
2019-01-17 12:04:31 +0100

    Extended TestSimpleValidator for async methods

--- FlexValidator.Tests/Validators/TestSimpleValidator.cs
+++ FlexValidator.Tests/Validators/TestSimpleValidator.cs
@@ -1,39 +1,64 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace FlexValidator.Tests.Validators {
     class TestSimpleValidator<T> : SimpleValidator<T> {
         public Action<T> ValidateFunc { get; set; }
+        public Func<T, Task> ValidateAsyncFunc { get; set; }
 
         /// <inheritdoc />
         protected override void DoValidate(T model) {
-            ValidateFunc(model);
+            ValidateFunc?.Invoke(model);
+        }
+
+        /// <inheritdoc />
+        protected override Task DoValidateAsync(T model) {
+            return ValidateAsyncFunc?.Invoke(model) ?? Task.CompletedTask;
         }
     }
 
     class TestSimpleValidator<T1, T2> : SimpleValidator<T1, T2> {
         public Action<T1, T2> ValidateFunc { get; set; }
+        public Func<T1, T2, Task> ValidateAsyncFunc { get; set; }
 
         /// <inheritdoc />
         protected override void DoValidate(T1 model1, T2 model2) {
-            ValidateFunc(model1, model2);
+            ValidateFunc?.Invoke(model1, model2);
+        }
+
+        /// <inheritdoc />
+        protected override Task DoValidateAsync(T1 model1, T2 model2) {
+            return ValidateAsyncFunc?.Invoke(model1, model2) ?? Task.CompletedTask;
         }
     }
 
     class TestSimpleValidator<T1, T2, T3> : SimpleValidator<T1, T2, T3> {
-        private Action<T1, T2, T3> ValidateFunc { get; set; }
+        public Action<T1, T2, T3> ValidateFunc { get; set; }
+        public Func<T1, T2, T3, Task> ValidateAsyncFunc { get; set; }
 
         /// <inheritdoc />
         protected override void DoValidate(T1 model1, T2 model2, T3 model3) {
-            ValidateFunc(model1, model2, model3);
+            ValidateFunc?.Invoke(model1, model2, model3);
+        }
+
+        /// <inheritdoc />
+        protected override Task DoValidateAsync(T1 model1, T2 model2, T3 model3) {
+            return ValidateAsyncFunc?.Invoke(model1, model2, model3) ?? Task.CompletedTask;
         }
     }
 
     class TestSimpleValidator<T1, T2, T3, T4> : SimpleValidator<T1, T2, T3, T4> {
-        private Action<T1, T2, T3> ValidateFunc { get; set; }
+        public Action<T1, T2, T3, T4> ValidateFunc { get; set; }
+        public Func<T1, T2, T3, T4, Task> ValidateAsyncFunc { get; set; }
 
         /// <inheritdoc />
         protected override void DoValidate(T1 model1, T2 model2, T3 model3, T4 model4) {
-            ValidateFunc(model1, model2, model3);
+            ValidateFunc?.Invoke(model1, model2, model3, model4);
+        }
+
+        /// <inheritdoc />
+        protected override Task DoValidateAsync(T1 model1, T2 model2, T3 model3, T4 model4) {
+            return ValidateAsyncFunc?.Invoke(model1, model2, model3, model4) ?? Task.CompletedTask;
         }
     }
 }
